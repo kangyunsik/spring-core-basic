5253d2c322bff34427f5b885d7be62014d961fa5
kangyunsik <re12io@ajou.ac.kr>
2021-08-23 01:36:12 +0900

    AppConfig create for separation of interests, native

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,18 +1,24 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
-    void join(){
+    void join() {
         //GIVEN
-        Member member = new Member(1L,"memberA",Grade.VIP);
+        Member member = new Member(1L, "memberA", Grade.VIP);
 
         //WHEN
         memberService.join(member);
