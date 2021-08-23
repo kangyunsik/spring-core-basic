5cafcb075e68b317b634e31a7d91d5eeeb676c39
kangyunsik <re12io@ajou.ac.kr>
2021-08-23 14:03:03 +0900

    Bean print test create

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,57 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName);
+            System.out.println("bean = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("어플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //ROLE_APPLICATION <- 커스텀(어플리케이션) 빈의 Role.
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName);
+                System.out.println("bean = " + bean);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("인프라 빈 출력하기")
+    void findInfraBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //ROLE_INFRASTRUCTURE <- 인프라 빈의 Role.
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName);
+                System.out.println("bean = " + bean);
+            }
+        }
+    }
+
+
+}
