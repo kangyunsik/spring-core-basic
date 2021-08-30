512224ff65c27b383bb45b57dc928dc7b8d8e248
kangyunsik <re12io@ajou.ac.kr>
2021-08-30 21:25:57 +0900

    Filter를 이용한 ComponentScan의 제외, 포함 경로 설정 Test

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,13 +1,12 @@
-package hello.core;
+package hello.core.scan;
 
+import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AutoAppConfigTest {
     @Test
